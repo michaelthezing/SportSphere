3eadc19dab97fb0d878917d58c607a7b1855b28d
michaelthezing <mtelezing23@gmail.com>
2024-09-08 01:03:04 -0700

    test12123

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA7O-b_5KDKXCV86eG6KD7gF-Q1P2dQYBA",
+    authDomain: "hot-takes-965ca.firebaseapp.com",
+    databaseURL: "https://hot-takes-965ca-default-rtdb.firebaseio.com",
+    projectId: "hot-takes-965ca",
+    storageBucket: "hot-takes-965ca.appspot.com",
+    messagingSenderId: "172295122616",
+    appId: "1:172295122616:web:9fbd169c0fd0ff9be88e4d",
+    measurementId: "G-5XR61BZ2QX"
+  };
